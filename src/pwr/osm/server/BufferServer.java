4d736dacc2481a5b24315d1b30da8d01c1e025fe
maciejsobotka <195420@student.pwr.edu.pl>
2014-11-04 19:18:59 +0100

    Initial commit. Stan z zeszłego semestru.

---
+++ src/pwr/osm/server/BufferServer.java
@@ -0,0 +1,44 @@
+//==============================================================================
+//	Serwer WWW
+//  - każda paczka od klientów obsługiwana jest przez osobny wątek
+//	  po otrzymaniu paczki tworzymy wątek do jej obsługi
+//==============================================================================
+package pwr.osm.server;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.net.*;
+import java.util.List;
+
+import pwr.osm.data.representation.MapPosition;
+@SuppressWarnings("resource")
+class BufferServer
+{
+	public final static int PORT = 9876;
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String args[]) throws Exception
+	{
+		Log log = new Log();
+		log.onStart();
+		System.out.println("SERVER WORKING");
+		System.out.println("waiting for packets...");
+		DatagramSocket serverUDPSocket = new DatagramSocket(PORT);   // Server WWW pod portem 9876
+        while(true)
+        	{	
+        		// odbior od klienta
+		      	byte[] receiveData = new byte[5000];
+    			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+    			serverUDPSocket.receive(receivePacket);			// odbior paczki		  
+				ByteArrayInputStream inputStream = new ByteArrayInputStream(receiveData);
+				ObjectInputStream is = new ObjectInputStream(new BufferedInputStream(inputStream));
+				List<MapPosition> geoPoints = (List<MapPosition>) is.readObject();
+    			System.out.println("From Client: " + geoPoints);    // odczyt zdania
+    			log.info("Got package, creating thread to handle");
+    			// wyslanie na server
+    			Runnable r = new ConnectionThread(geoPoints, receivePacket.getAddress(), receivePacket.getPort());
+    			new Thread(r).start();
+           }
+	}
+}
---
+++ src/pwr/osm/server/Log.java
@@ -0,0 +1,66 @@
+package pwr.osm.server;
+
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+
+public class Log
+{
+	private PrintWriter logFile;
+	private SimpleDateFormat df = new SimpleDateFormat("[HH:mm:ss] ");
+
+	public void writeLog(String nfo)
+	{
+		Date time = new Date();
+		try
+		{
+			logFile = new PrintWriter(new BufferedWriter(new FileWriter("LOG.txt", true)));
+		} catch (IOException e) {
+			System.err.println("IOException: " + e.getMessage());
+		}
+		logFile.println(df.format(time) + nfo);
+		logFile.close();
+	}
+	public void info(String nfo)
+	{
+		writeLog("INFO: " + nfo);
+	}
+	public void error(String nfo)
+	{
+		writeLog("ERROR: " + nfo);
+	}
+	public void warn(String nfo)
+	{
+		writeLog("WARNING: " + nfo);
+	}
+    public void onStart()
+    {
+    	Date time = new Date();
+		try
+		{
+			logFile = new PrintWriter(new BufferedWriter(new FileWriter("LOG.txt", true)));
+		} catch (IOException e) {
+			System.err.println("IOException: " + e.getMessage());
+		}
+    	logFile.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    	logFile.println(df.format(time) + "INFO: Program Start");
+    	logFile.close();
+    }
+    public void onClose()
+    {
+    	Date time = new Date();
+		try
+		{
+			logFile = new PrintWriter(new BufferedWriter(new FileWriter("LOG.txt", true)));
+		} catch (IOException e) {
+			System.err.println("IOException: " + e.getMessage());
+		}
+    	logFile.println(df.format(time) + "INFO: Program End");
+    	logFile.println("-----------------------------------------------------------");
+    	logFile.close();
+    }
+}
