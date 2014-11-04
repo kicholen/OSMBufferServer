f478a1cbadde0befda0c9f24c5da26a49f37fdd6
maciejsobotka <195420@student.pwr.edu.pl>
2014-11-04 19:24:45 +0100

    Dodanie klas do obsługi bazy danych. Potrzebne biblioteki nie zostały dołączone.

---
+++ src/pwr/osm/db/DbMapPosition.java
@@ -0,0 +1,64 @@
+package pwr.osm.db;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="DBMAPPOSITION")
+public class DbMapPosition{
+	
+	@Id
+	@GeneratedValue
+	@Column(name="position_id")
+	private Long positionId;
+	
+	@Column(name="latitude")
+	private double latitude;
+	
+	@Column(name="longitude")
+	private double longitude;
+	
+	@ManyToOne
+	@JoinColumn(name="path_id", insertable=false, updatable=false, nullable=false)
+	private DbPath dbPath;
+
+	// Constructor
+	public DbMapPosition() {
+
+	}
+	
+	public DbMapPosition(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	// Getter and Setter methods	
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	public DbPath getDbPath(){
+		return dbPath;
+	}
+	
+	public void setDbPath(DbPath dbPath){
+		this.dbPath = dbPath;
+	}
+}
---
+++ src/pwr/osm/db/DbPath.java
@@ -0,0 +1,103 @@
+package pwr.osm.db;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="DBPATH")
+public class DbPath {
+
+	@Id
+	@GeneratedValue
+	@Column(name="path_id")
+	private Long pathId;
+	
+	@Column(name="start_latitude")
+	private double startLatitude;
+	
+	@Column(name="start_longitude")
+	private double startLongitude;
+
+	@Column(name="end_latitude")
+	private double endLatitude;
+	
+	@Column(name="end_longitude")
+	private double endLongitude;
+	
+	@OneToMany(cascade={CascadeType.ALL})
+    @JoinColumn(name="path_id")
+    @OrderColumn(name="idx")
+	private List<DbMapPosition> dbPathPoints;
+	
+	// Constructor
+	public DbPath() {
+		
+	}
+	
+	public DbPath(double startLatitude, double startLongitude, double endLatitude, double endLongitude) {
+		this.startLatitude = startLatitude;
+		this.startLongitude = startLongitude;
+		this.endLatitude = endLatitude;
+		this.endLongitude = endLongitude;
+	}
+	
+	// Getter and Setter methods
+	public Long getPathId() {
+		return pathId;
+	}
+
+	public void setPathId(Long pathId) {
+		this.pathId = pathId;
+	}
+
+	public double getStartLatitude() {
+		return startLatitude;
+	}
+
+	public void setStartLatitude(double startLatitude) {
+		this.startLatitude = startLatitude;
+	}
+
+	public double getStartLongitude() {
+		return startLongitude;
+	}
+
+	public void setStartLongitude(double startLongitude) {
+		this.startLongitude = startLongitude;
+	}
+
+	public double getEndLatitude() {
+		return endLatitude;
+	}
+
+	public void setEndLatitude(double endLatitude) {
+		this.endLatitude = endLatitude;
+	}
+
+	public double getEndLongitude() {
+		return endLongitude;
+	}
+
+	public void setEndLongitude(double endLongitude) {
+		this.endLongitude = endLongitude;
+	}
+
+	public List<DbMapPosition> getDbPath() {
+		return dbPathPoints;
+	}
+
+	public void setDbPath(List<DbMapPosition> dbPathPoints) {
+		this.dbPathPoints = dbPathPoints;
+	}
+	
+
+}
