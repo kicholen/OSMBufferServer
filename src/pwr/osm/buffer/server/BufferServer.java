b201a79c33f255a1ad995ef402c54b347b8c88a8
maciejsobotka <195420@student.pwr.edu.pl>
2014-12-07 02:24:03 +0100

    Refaktoryzacja klasy BufferServer.

--- src/pwr/osm/buffer/server/BufferServer.java
+++ src/pwr/osm/buffer/server/BufferServer.java
@@ -1,5 +1,6 @@
 package pwr.osm.buffer.server;
 
+import java.io.IOException;
 import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,27 +16,33 @@ import pwr.osm.buffer.util.Log;
 class BufferServer
 {
 	private final static int PORT = 9876;
-	private static long requestId = 1;
 	
-	public static void main(String args[]) throws Exception
+	public static void main(String args[])
 	{
-		ExecutorService execService = Executors.newFixedThreadPool(11);
+		ExecutorService execService = Executors.newFixedThreadPool(21);
+		long requestId = 1;
 		Log log = new Log();
 		log.onStart();
 		System.out.println("SERVER WORKING");
 		System.out.println("waiting for packets...");
-		@SuppressWarnings("resource")
-		DatagramSocket serverUDPSocket = new DatagramSocket(PORT);   // BufferServer port: 9876
-		execService.execute(new DbClearThread(1, 2));
-        while(true)
-    	{	
-	      	byte[] receiveData = new byte[5000];
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			serverUDPSocket.receive(receivePacket);
-			System.out.println("Got package, creating threads to handle");
-			log.info("Got package, creating thread to handle");		
-			execService.execute(new RequestHandler(requestId, receivePacket, receiveData));
-			requestId++;
-       }
+		DatagramSocket serverUDPSocket;
+		try {
+			serverUDPSocket = new DatagramSocket(PORT);
+
+			execService.execute(new DbClearThread(1, 2));
+	        while(true)
+	    	{	
+	        	byte[] receiveData = new byte[5000];
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				serverUDPSocket.receive(receivePacket);
+				System.out.println("Got package, creating threads to handle");
+				log.info("Got package, creating thread to handle");		
+				execService.execute(new RequestHandler(requestId, receivePacket, receiveData));
+				requestId++;
+    	}
+		} catch (IOException e) {
+			log.equals(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
